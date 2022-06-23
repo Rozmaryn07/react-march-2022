e8b3abe74b19db2e53b3f3dfdff5cd8bb16feada
Rozmaryn07 <taras.marhalevich@gmail.com>
2022-06-23 07:01:37 +0300

    hw2.1

--- src/components/Starts.js
+++ src/components/Starts.js
@@ -7,15 +7,14 @@ export default function Starts() {
   useEffect(()=>{
     fetch("https://api.spacexdata.com/v3/launches/")
         .then(value => value.json())
-        .then(value =>{if (value.launch_year!== 2020){setStarts(value)}})
-    // setStarts(value.filter(()=>flight.launch_year!== 2020) ))
+        .then(value =>setStarts(value.filter(value=>value.launch_year!== '2020')))
   },[]);
   const fligh_tdetail = (flight) => {
     setflight( flight);
   };
   return (<div >
 
-    {flight.flight_number && <div> {flight.mission_name} {flight.launch_year} {<img src={flight.links.mission_patch_small}/>} </div>}
+    {flight.flight_number && <div> {flight.mission_name} {flight.launch_year} {<img src={flight.links.mission_patch_small} alt="from HIMARS with love"   />} </div>}
     <h2>spacexdata</h2>
 
     {Starts.map((flight, index) =><Flight
