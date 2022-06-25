4d4bbb2e0f4aa79a903606c93212a14ed5728c07
Rozmaryn07 <taras.marhalevich@gmail.com>
2022-06-26 01:45:03 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import './App.css';
-// eslint-disable-next-line no-unused-vars
-function App() {
-  return (
-    <div >
-
-
+export default function App() {
+  return (<div >
+      
     </div>);
 }
-
-export default App;
