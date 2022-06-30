60014c0ad92450cd167acf592803bb45d90bd9cf
Rozmaryn07 <taras.marhalevich@gmail.com>
2022-06-30 12:22:22 +0300

    hw

--- src/App.js
+++ src/App.js
@@ -7,3 +7,7 @@ function App() {
 }
 
 export default App;
+// Зробити компонент, в якому буде форма, за допомоги якої можливо створити нового юзера
+// постовим запитом на http://jsonplaceholder.typicode.com/users
+//     Зробити компонент, в якому буде форма, за допомоги якої можливо створити новий комментар
+//     постовим запитом на http://jsonplaceholder.typicode.com/comments
